0ec0eb32b30004d10f66533c6938fd207eda2ce5
Dafna <margalit.dafna77@gmail.com>
2021-10-21 11:44:18 -0600

    letter vertices

--- main.c
+++ main.c
@@ -1,124 +1,99 @@
-
-// A C Program to demonstrate adjacency list 
-// representation of graphs
 #include <stdio.h>
 #include <stdlib.h>
-  
-// A structure to represent an adjacency list node
+#include <string.h>
+
 struct AdjListNode
 {
-    int dest;
+    char *dest;
     struct AdjListNode* next;
 };
-  
-// A structure to represent an adjacency list
+
 struct AdjList
 {
-    struct AdjListNode *head; 
+    struct AdjListNode *head;  // pointer to head node of list
 };
-  
-// A structure to represent a graph. A graph
-// is an array of adjacency lists.
-// Size of array will be V (number of vertices 
-// in graph)
+
 struct Graph
 {
     int V;
     struct AdjList* array;
 };
-  
-// A utility function to create a new adjacency list node
-struct AdjListNode* newAdjListNode(int dest)
+
+struct AdjListNode* newAdjListNode(char *dest)
 {
-    struct AdjListNode* newNode =
-     (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
+    struct AdjListNode* newNode = (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
     newNode->dest = dest;
     newNode->next = NULL;
     return newNode;
 }
-  
-// A utility function that creates a graph of V vertices
+
 struct Graph* createGraph(int V)
 {
-    struct Graph* graph = 
-        (struct Graph*) malloc(sizeof(struct Graph));
+    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
     graph->V = V;
-  
-    // Create an array of adjacency lists.  Size of 
-    // array will be V
-    graph->array = 
-      (struct AdjList*) malloc(V * sizeof(struct AdjList));
-  
-    // Initialize each adjacency list as empty by 
-    // making head as NULL
+
+    // Create an array of adjacency lists.  Size of array will be V
+    graph->array = (struct AdjList*) malloc(V * sizeof(struct AdjList));
+
+    // Initialize each adjacency list as empty by making head as NULL
     int i;
     for (i = 0; i < V; ++i)
         graph->array[i].head = NULL;
-  
+
     return graph;
 }
-  
-// Adds an edge to an undirected graph
-void addEdge(struct Graph* graph, int src, int dest)
+
+void addEdge(struct Graph* graph, char *src, char *dest)
 {
-    // Add an edge from src to dest.  A new node is 
-    // added to the adjacency list of src.  The node
-    // is added at the beginning
+    // Add an edge from src to dest.  A new node is added to the adjacency
+    // list of src.  The node is added at the beginin
+
     struct AdjListNode* newNode = newAdjListNode(dest);
-    newNode->next = graph->array[src].head;
-    graph->array[src].head = newNode;
-  
-    // Since graph is undirected, add an edge from
-    // dest to src also
+
+    //***Changed. you need to add edge in src node. But you were always adding in 0
+    newNode->next = graph->array[src[0]-'a'].head;
+    graph->array[src[0]-'a'].head = newNode;
+
+    // Since graph is undirected, add an edge from dest to src also
     newNode = newAdjListNode(src);
-    newNode->next = graph->array[dest].head;
-    graph->array[dest].head = newNode;
+
+    //***Changed. you need to add edge in dest node. But you were always adding in 1
+    newNode->next = graph->array[dest[0]-'a'].head;
+    graph->array[dest[0]-'a'].head = newNode;
 }
-  
-// A utility function to print the adjacency list 
-// representation of graph
+
 void printGraph(struct Graph* graph)
 {
     int v;
     for (v = 0; v < graph->V; ++v)
     {
         struct AdjListNode* pCrawl = graph->array[v].head;
-        printf("\n Adjacency list of vertex %d\n head ", );
+        printf("\n Adjacency list of vertex %d\n head %c", v, v + 'a');
         while (pCrawl)
         {
-            printf("-> %d", pCrawl->dest);
+            printf("-> %s", pCrawl->dest);
             pCrawl = pCrawl->next;
         }
         printf("\n");
     }
 }
-  
-// Driver program to test above functions
+
 int main()
 {
     // create the graph given in above fugure
-    int V = 12;
+    int V = 5;
     struct Graph* graph = createGraph(V);
+    addEdge(graph, "a", "b");
+    addEdge(graph, "a", "e");
+    addEdge(graph, "b", "c");
+    addEdge(graph, "b", "d" );
+    addEdge(graph, "b", "e");
+    addEdge(graph, "c", "d");
+    addEdge(graph, "d", "e");
 
-    addEdge(graph, 0, 1);
-    addEdge(graph, 0, 200);
-    addEdge(graph, 0, 500);
-    addEdge(graph, 1, 2);
-    addEdge(graph, 2, 200);
-    addEdge(graph, 2, 100);
-    addEdge(graph, 2, 3);
-    addEdge(graph, 3, 4);
-    addEdge(graph, 3, 5);
-    addEdge(graph, 3, 6);
-    addEdge(graph, 4, 200);
-    addEdge(graph, 4, 300);
-    addEdge(graph, 5, 100);
-    addEdge(graph, 5, 400);
-    addEdge(graph, 6, 400);
-    addEdge(graph, 6, 300);
-  
+
     // print the adjacency list representation of the above graph
     printGraph(graph);
-  
+
     return 0;
 }
