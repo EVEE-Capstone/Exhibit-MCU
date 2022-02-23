cdb7df15b31f2b5459cd318f30106f0fe3be128f
Max Pettit <max@estarwest.com>
2022-02-22 17:08:43 -0700

    Arduino support in usart files

--- exhibit-leuart-pg/src/usart.h
+++ exhibit-leuart-pg/src/usart.h
@@ -18,10 +18,12 @@
 
 // BLE RX connects to SPI_MOSI
 // BLE TX connects to SPI_MISO
-#define BLE_USART   USART2
+#define BLE_USART   USART2      // USART2
 #define BLE_STARTFRAME  '#'
 #define BLE_SIGFRAME    '!'
 
+// ARDUINO COMMUNICATION
+#define ARD_USART   USART3
 
 typedef struct {
   uint32_t          rx_loc;
@@ -32,6 +34,7 @@ typedef struct {
   USART_Stopbits_TypeDef        stopbits;
   USART_Databits_TypeDef        databits;
   bool              msbf;
+  bool              rx_en;
 } USART_OPEN_STRUCT;
 
 
@@ -48,10 +51,16 @@ typedef struct {
   char                  string[80];
   uint32_t              string_len;
   uint32_t              counter;
+  uint32_t              rxdataValid;
   volatile bool         sm_busy;      // If true state machine can't be restarted
 } USART_STATE_MACHINE_STRUCT;
 
 
+
+void ard_usart_open(void);
+void ard_write(char* string);
+void ard_read(char * str);
+bool ard_newData(void);
 
 void ble_usart_open(void);
 bool usart_tx_busy(USART_TypeDef *usart);
